caff8153e3d5a36ea7e498b5623afbe0aa3a9b34
rajivsam <rajiv@arangodb.com>
2024-02-07 15:19:17 +0530

    Initial baseline for doc strings

--- kmds/meta_data_logger.py
+++ kmds/meta_data_logger.py
@@ -6,6 +6,12 @@ from utils.path_utils import get_ontology_path, get_kb_file_path
 class MetaDataLogger:
 
     def __init__(self, pipeline_name: str, piplline_type: PipelineType) -> None:
+        """ Class abstraction to log observations to the knowledge base
+
+        Args:
+            pipeline_name (str): the pipeline name to log to
+            piplline_type (PipelineType): the type of pipeline - knowledge application or knowledge extraction experiment
+        """
         self._pipeline_name: str = pipeline_name
         self._pipeline_type: str = piplline_type
 
@@ -19,6 +25,11 @@ class MetaDataLogger:
         return
     
     def save_knowledge_base(self, file_name: str) -> None:
+        """ Save the knowledge base to the supplied file name
+
+        Args:
+            file_name (str): The file name to save the knowledge base to
+        """
         assert file_name is not None
         onto = get_ontology(get_ontology_path()).load()
         onto.save(file=get_kb_file_path(file_name=file_name), format="rdfxml")
@@ -27,17 +38,32 @@ class MetaDataLogger:
 
 
     def log_exploratory_observations(self, exploratory_findings: List[ExploratoryObservation]) -> None:
+        """ Log the list of exploratory observations to the ontology
+
+        Args:
+            exploratory_findings (List[ExploratoryObservation]): List of exploratory observations
+        """
         self._aw.has_exploratory_observations = exploratory_findings
 
         return
 
     def log_data_representation_observations(self, data_representation_obs: List[DataRepresentationObservation]) -> None:
+        """ Log the list of data representation observations to the ontology
+
+        Args:
+            data_representation_obs (List[DataRepresentationObservation]): List of data representation observations received
+        """
         self._aw.has_data_representation_observations = data_representation_obs
 
 
         return
 
     def log_modelling_choice_observations(self, modelling_choice_obs: List[ModellingChoiceObservation]) -> None:
+        """ Log modelling choice observations to the ontology
+
+        Args:
+            modelling_choice_obs (List[ModellingChoiceObservation]): list of modelling choice observations
+        """
 
         for obs in modelling_choice_obs:
             obs.is_modelling_choice_observation_of_type.append(obs.obs_type)
@@ -48,6 +74,11 @@ class MetaDataLogger:
         return
     
     def log_model_selection_observations(self, model_selection_obs: List[ModellingChoiceObservation]) -> None:
+        """ Log model selection observations to the ontology
+
+        Args:
+            model_selection_obs (List[ModellingChoiceObservation]): list of model selection observations
+        """
 
         for obs in model_selection_obs:
             obs.is_model_selection_observation_of_type.append(obs.obs_type)
--- kmds/utils/path_utils.py
+++ kmds/utils/path_utils.py
@@ -2,8 +2,21 @@ from pathlib import Path
 import os
 
 def get_ontology_path() ->Path:
+    """ Return the ontology file path as a Path
+
+    Returns:
+        Path: the path to the ontology file 
+    """
     return os.path.join(Path(__file__).absolute().parent.parent, "ontology/kmds_ontology.xml")
 
 def get_kb_file_path(file_name: str) ->Path:
+    """ Given a file name for a knowledge base, return the path to it as a Path
+
+    Args:
+        file_name (str): the filename of the knowledge base
+
+    Returns:
+        Path: the path corresponding to the provided file name
+    """
     kb_segment = "data/" + file_name + ".xml"
     return os.path.join(Path(__file__).absolute().parent.parent.parent, kb_segment) 
