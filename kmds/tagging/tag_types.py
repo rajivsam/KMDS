a93d3d88b8419ed70324368b48fb998b87968339
rajivsam <rajiv@arangodb.com>
2024-02-07 15:09:15 +0530

    Initial baseline for doc strings

--- kmds/tagging/tag_types.py
+++ kmds/tagging/tag_types.py
@@ -1,24 +1,54 @@
 from enum import Enum
 
 class ExploratoryTags(str, Enum):
+    """ Tags to capture the exploratory observation type, the first tag must be applied (if applicable)prior to the second tag - so relevance must be determined (if you want to specify relevance observations) prior to data quality checks
+
+    Args:
+        str (_type_): string type tag
+        Enum (_type_): string type tag
+    """
     RELEVANCE_OBSERVATION = "Relevance Observation" 
     DATA_QUALITY_OBSERVATION = "Data Quality Observation"
 
 class DataRepresentationTags(str, Enum):
+    """ Tags to capture the data representation observation type, the first tag must be applied if applicable prior to the second tag
+
+    Args:
+        str (_type_): string type tag
+        Enum (_type_): string type tag
+    """
     FEATURE_ENGG_OBSERVATION = "Feature Engineering Observation"
     DATA_TRANSFORMATION_OBSERVATION = "Data Transformation Observation"
 
 class ModellingChoiceTags(str, Enum):
+    """ Tags corresponding to Modelling choice observation type, the tag precedence is in the order of definition
+
+    Args:
+        str (_type_): string type tag
+        Enum (_type_): string type tag
+    """
     MODELLING_CHOICE_OBSERVATION = "Modelling Choice Observation"
     MODELLING_ASSUMPTION_OBSERVATION = "Modelling Assumption Observation"
 
 class ExperimentationTags(str, Enum):
+    """ Tags corresponding to experimentation observation type, the tag precedence is in the order of definition
+
+    Args:
+        str (_type_): string type tag
+        Enum (_type_): string type tag
+    """
     HYPOTHESIS_STATEMENT = "Hypothesis Statement"
     EXPERIMENTAL_OBSERVATION = "Experimental Observation"
     EXPERIMENTAL_CONJECTURE = "Experimental Conjecture"
     RESULT_SUMMARY = "Experimental Result Summary"
 
 class ModelSelection(str, Enum):
+    """ Tags corresponding to Model Selection observation type, the tag precedence is in the order of definition
+
+    Args:
+        str (_type_): string type tag
+        Enum (_type_): string type tag
+    """
     MODEL_SELECTION_STATEMENT = "Model Selection Statement"
     MODEL_SELECTION_SETUP_DESCRIPTION = "Model Selection Setup Description"
     MODEL_SELECTION_OBSERVATION = "Model Selection Observation"
